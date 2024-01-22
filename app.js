2db50ccd3cc70b48d3c1ce9c5334b939d94ae54b
javis <javisch012@gmail.com>
2024-01-22 00:02:17 -0300

    agrego la plantilla html del header y footer

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path')
+
 const app = express();
 const PORT = 3030;
-const path = require('path')
+
+// ACCESO A LOS ARCHIVOS DE LA CARPETA public 
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, './views'));
 
@@ -9,13 +15,19 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/header_and_footer', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/header_and_footer.html'))
+});
+
 app.get('/iniciar-sesion', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/login.html'));
 
 });
+
 app.get('/crear-cuenta', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/register.html'))
 });
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 })
